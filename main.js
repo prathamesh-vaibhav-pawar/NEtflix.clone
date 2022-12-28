b13b647cbba8a7725690798d77efd7568b91efd1
prathamesh-vaibhav-pawar <107491217+prathamesh-vaibhav-pawar@users.noreply.github.com>
2022-12-28 20:54:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function signup(){
+    window.open("https://www.netflix.com/in/login");
+}
+User_name = document.getElementById("in1").value 
+function User(){
+    localStorage.setItem("User",User_name)
+}
